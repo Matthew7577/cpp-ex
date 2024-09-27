ba023e0bdf94657e83a6aeab6ec3fe42aae6eafa
LittleBlue7577 <matthew7577@gmail.com>
2024-09-27 15:52:02 +0800

    update

---
+++ Ex3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    int x1, x2;
+    std::cout << "Enter the first integer: ";
+    std::cin >> x1;
+    std::cout << "Enter the second integer:";
+    std::cin >> x2;
+    return 0;
+}
