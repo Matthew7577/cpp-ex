daf914abc7e599d11278e20fdf8dab64521c97ae
LittleBlue7577 <matthew7577@gmail.com>
2024-09-27 15:43:20 +0800

    update ex

--- Ex2a.cpp
+++ Ex2a.cpp
@@ -3,10 +3,18 @@ using namespace std;
 int main()
 {
     int units;
+    int total;
     cout << "Input Value: ";
     cin >> units;
     if (units <= 0)
-        cout << "Input error";
+    {
+        cout << "Input error\n";
+    }
     else
+    {
+        total = units * 5;
         cout << "Unit = " << units << "\n";
+        cout << "The total is: " << total;
+    }
+    return 0;
 }
