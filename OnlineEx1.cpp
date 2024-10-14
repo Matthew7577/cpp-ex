7c6d81c09b7de051db82ae0fa1f58afc87af75bf
LittleBlue7577 <matthew7577@gmail.com>
2024-10-14 23:55:13 +0800

    New online exercise

---
+++ OnlineEx1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ int main(){
+    int a=10;
+    int b=3;
+    int c,d;
+    c=a*b;
+    d=b%a;
+    std::cout << c << "\n";
+    std::cout << d << "\n";
+    return 0;
+ }
---
+++ OnlineEx2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float x, y, w;
+    cout << "Input x: ";
+    cin >> x;
+    cout << "Input y: ";
+    cin >> y;
+    if (x == 0 && y == 0){
+        cout << "Error! Both numbers are zero!";
+    }
+    else{
+        w = (x*x+y*y)/((2*x+y*y)*(2*y-x*x));
+        cout << "The output w equals to " << w;
+    }
+    return 0;
+}
