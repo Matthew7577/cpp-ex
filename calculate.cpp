3cddbf0c84988fe32f605763f1dad0914efb32e4
LittleBlue7577 <matthew7577@gmail.com>
2025-02-14 19:45:32 +0800

    Fix calculation logic in calculate.cpp to square the answer

--- calculate.cpp
+++ calculate.cpp
@@ -7,7 +7,8 @@ int main()
     double answer;
     cout << "Input the value of x: ";
     cin >> x;
-    answer = (((3 + 4 * x) / 10.0) - ((10 * (x - 2) * (x - 2) ) / (x - 3.0))) * (((3 + 4 * x) / 10.0) - ((10 * (x - 2) * (x - 2) ) / (x - 3.0)));
+    answer = (((3 + 4 * x) / 10.0) - ((10 * (x - 2) * (x - 2) ) / (x - 3.0)));
+    answer *= answer;
     cout << "The answer is " << answer;
     return 0;
 }
