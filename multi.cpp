0f04b8fa544eda93514ab86c4c01c93c5adaa0c3
LittleBlue7577 <matthew7577@gmail.com>
2025-02-19 21:38:30 +0800

    Fix formatting in multiplication table output in multi.cpp

--- multi.cpp
+++ multi.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int i, j;
     cout << "                 Multiplication Table" << endl;
-    cout << "     |    1    2    3    4    5    6    7    8    9  " << endl;
+    cout << "     |    1    2    3    4    5    6    7    8    9" << endl;
     cout << "-----+----------------------------------------------" << endl;
     for (i = 1; i < 10; i++)
     {
