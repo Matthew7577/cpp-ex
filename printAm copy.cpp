1ed1e236486b84cfd0ae32886e9fc2681541ea17
LittleBlue7577 <matthew7577@gmail.com>
2025-02-19 22:11:38 +0800

    Add printAm program to generate a pattern based on user input

---
+++ printAm copy.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int i, j;
+    cin >> j;
+    if (j % 2 == 0 || j < 0 || j == 0) {
+        cout << "E";
+    } else {
+        for (i = 0; i < j; i++)
+        {
+            int x = i + i - 1;
+            cout << right << setw(j - i) << "*";
+            while (x > 0){
+                cout << "=";
+                x--;
+            }
+            if (i > 0){
+                cout << "*";
+            }
+            cout << endl;
+        }
+        
+    }
+}
